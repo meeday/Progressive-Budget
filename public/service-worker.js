d7006238b7b6168981303b3a4524a933cf47741a
meeday <59993824+meeday@users.noreply.github.com>
2020-07-28 15:47:32 +0100

    activate handler function created

--- public/service-worker.js
+++ public/service-worker.js
@@ -19,3 +19,25 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+  event.waitUntil(
+    caches
+    .keys()
+    .then(cacheNames => {
+      return cacheNames.filter(
+        cacheName => !currentCaches.includes(cacheName)
+      );
+    })
+    .then(cachesToDelete => {
+      return Promise.all(
+        cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        })
+      );
+    })
+    .then(() => self.Clients.claim())
+  );
+});
+
+
