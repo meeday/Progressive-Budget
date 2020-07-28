493b87fc23b6b53ba41973f4f6b9a29976d184df
meeday <59993824+meeday@users.noreply.github.com>
2020-07-28 15:52:40 +0100

    fetch handler created

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,40 +4,67 @@ const FILES_TO_CACHE = [
   "/styles.css",
   "/dist/bundle.js",
   "/icons/icon-192x192.png",
-  "/icons/icon-192x192.png"
+  "/icons/icon-192x192.png",
 ];
 
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(cache => cache.addAll(FILES_TO_CACHE))
-    .then(() => self.slipWaiting())
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.slipWaiting())
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
   event.waitUntil(
     caches
-    .keys()
-    .then(cacheNames => {
-      return cacheNames.filter(
-        cacheName => !currentCaches.includes(cacheName)
-      );
-    })
-    .then(cachesToDelete => {
-      return Promise.all(
-        cachesToDelete.map(cacheToDelete => {
-          return caches.delete(cacheToDelete);
-        })
-      );
-    })
-    .then(() => self.Clients.claim())
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.Clients.claim())
   );
 });
 
+self.addEventListener("fetch", event => {
+  if(
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
+  if(event.request.url.includes("/api/transaction")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+        .then(response => {
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+  
+
+
+
+
+})
