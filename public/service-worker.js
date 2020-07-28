eee9cd415d15a488f17ea175270f6a890816e753
meeday <59993824+meeday@users.noreply.github.com>
2020-07-28 15:43:21 +0100

    install handler created

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,5 +10,12 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+    .open(CACHE_NAME)
+    .then(cache => cache.addAll(FILES_TO_CACHE))
+    .then(() => self.slipWaiting())
+  );
+});
 
